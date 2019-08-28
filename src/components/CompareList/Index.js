597dbe48bc0ca1871a835bfbb48111a827d64d3f
Esio Freitas <esio.gustavo@gmail.com>
2019-08-27 22:27:05 -0300

    Add compareList component

---
+++ src/components/CompareList/Index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const CompareList = ()=>(
+  <div>
+    <div>
+      <header>
+        <img src="" alt="facebook"/>
+        <strong>react</strong>
+        <strong>facebook</strong>
+      </header>
+      <ul>
+        <li>
+          95,8 <small>stars</small>
+        </li>
+      </ul>
+    </div>
+  </div>
+);
+
+export default CompareList;
